024c924dfd40c4db12681989d6bb15f7ed34fc2b
DonSqualo <90971750+DonSqualo@users.noreply.github.com>
2025-04-02 01:18:21 +0100

    fix bug with subchapter search

--- assets/js/map.js
+++ assets/js/map.js
@@ -355,16 +355,26 @@ if (
           const link = document.createElement('a');
           link.href = "#" + result.item.anchor;
           link.addEventListener("click", (e) => {
+            window.closeMap();
             if (result.matches[0].key == "content.explanations.text") {
               e.preventDefault();
               const section = document.getElementById(result.item.anchor)
-              window.scrollTo(0, section.getBoundingClientRect().top + window.scrollY + window.innerHeight)
-              explanationLink = section.querySelectorAll('a[href^="#"]').filter()
-              section.quer
-              entry.classList.toggle('active')
-              explanationLinks[index].classList.toggle('active')
+              console.log(result, section, result.matches[0].refIndex, section.querySelectorAll('a[href^="#"]'))
+              const explanationLink = section.querySelectorAll('a[href^="#"]')[result.matches[0].refIndex]
+              const explanation = section.querySelectorAll('.mo-explanation')[result.matches[0].refIndex]
+              explanationLink.classList.toggle('active')
+              explanation.classList.toggle('active')
+
+              window.scrollTo(0, section.getBoundingClientRect().bottom + window.scrollY - window.innerHeight)
+              explanationLink.scrollTo({
+                top: section.querySelector(".mo-main-content").offsetTop - 15,
+                behavior: 'smooth'
+              });
+              explanationLink.scrollTo({
+                top: section.querySelector(".mo-translation").offsetTop - 9,
+                behavior: 'smooth'
+              });
             }
-            window.closeMap();
           })
           const thumb = document.createElement('img');
           thumb.src = result.item.thumbnail;
